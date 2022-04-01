b4f3a29c9f18794a602f23bff8b7a023a49addfd
anonacc2 <102777099+anonacc2@users.noreply.github.com>
2022-04-01 17:58:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 String.prototype.repAll = function(from,to) {
   return this.split(from).join(to)
 }
+function picoPerMillisecond() {
+  return 0.001/0.000000000001
+}
+  
+ppm = picoPerMillisecond()
 function time(format) {
   let f = format;
   f = f.repAll("y", new Date().getFullYear())
   f = f.repAll("M", new Date().getMonth()+1)
   f = f.repAll("d", new Date().getDate())
-  f = f.repAll("h", new Date().getHours())
+  if (new Date().getHours() > 12) {
+    f = f.repAll("h", new Date().getHours()-12)
+  } else {
+    f = f.repAll("h", new Date().getHours())
+   }
   f = f.repAll("m", new Date().getMinutes())
   f = f.repAll("s", new Date().getSeconds())
   f = f.repAll("S", new Date().getMilliseconds())
+  f = f.repAll("p", new Date().getMilliseconds()*ppm())
   return f
 }
 
 let t = time;
-
