22aee0dabcc755dd05fd58dba9b73f8bc6dc7904
anonacc2 <102777099+anonacc2@users.noreply.github.com>
2022-03-31 20:40:10 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+String.prototype.repAll = function(from,to) {
+  return this.split(from).join(to)
+}
+function time(format) {
+  let f = format;
+  f = f.repAll("y", new Date().getFullYear())
+  f = f.repAll("M", new Date().getMonth()+1)
+  f = f.repAll("d", new Date().getDate())
+  f = f.repAll("h", new Date().getHours()-12)
+  f = f.repAll("m", new Date().getMinutes())
+  f = f.repAll("s", new Date().getSeconds())
+  f = f.repAll("ms", new Date().getMilliSeconds())
+  return f
+}
